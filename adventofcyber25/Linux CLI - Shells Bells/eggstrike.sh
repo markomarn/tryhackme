4bb31526ea304b8f03408dbdb64875399695fa9c
markomar <kohanamei11@gmail.com>
2025-12-14 19:33:50 +0700

    artifacts

---
+++ adventofcyber25/Linux CLI - Shells Bells/eggstrike.sh
@@ -0,0 +1,8 @@
+# Eggstrike v0.3
+# © 2025, Sir Carrotbane, HopSec
+cat wishlist.txt | sort | uniq > /tmp/dump.txt
+rm wishlist.txt && echo "Chistmas is fading..."
+mv eastmas.txt wishlist.txt && echo "EASTMAS is invading!"
+
+# Your flag is:
+# THM{sir-carrotbane-attacks}
